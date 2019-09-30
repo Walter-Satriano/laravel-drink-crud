00dcb6df2bd9d29b30891575ffb4285b36e8f37a
Walter Satriano <walter.satri@gmail.com>
2019-09-30 17:18:01 +0200

    aggiunta section con min, max e media

--- resources/views/bevande.blade.php
+++ resources/views/bevande.blade.php
@@ -1,7 +1,6 @@
 @extends("layouts.bevande-layout")
 
 @section("content")
-
   @foreach ($bevande as $bevanda)
     <div class="bev_list">
       <p>{{ $bevanda -> nome_bevanda}}</p>
@@ -10,5 +9,19 @@
       <p>{{ $bevanda -> data_di_scadenza}}</p>
     </div>
   @endforeach
+@endsection
 
+
+@section("calc")
+  <div class="calc_list">
+    <p>PREZZO MINIMO: <br> {{ $min }}</p>
+  </div>
+
+  <div class="calc_list">
+    <p>PREZZO MASSIMO: <br> {{ $max }}</p>
+  </div>
+
+  <div class="calc_list">
+    <p>MEDIA PREZZI: <br> {{ $avg }}</p>
+  </div>
 @endsection
